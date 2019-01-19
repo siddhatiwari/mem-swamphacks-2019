6e771be5c9ba0bb9915bd110493eda850936b44e
Zachary Cowan <zcowan@ufl.edu>
2019-01-19 04:56:52 -0500

    Face detection

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions, FileSystem, BarCodeScanner, ImageManipulator } from 'expo';
+import { Camera, Permissions, FileSystem, BarCodeScanner, ImageManipulator, FaceDetector } from 'expo';
 import { Dimensions } from 'react-native';
 
 const dimensions = Dimensions.get('window');
 const windowWidth = dimensions.width;
 const windowHeight = dimensions.height;
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container} >
-        <Camera
-          style={{ height: windowHeight, width: windowWidth }} 
-          type={Camera.Constants.Type.back}
-          ref={ref => this.camera = ref}>
-          <View>
-            <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:0,bottom:0,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Zach!')}>
-            </TouchableOpacity>
-            <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:0,left:windowWidth/2,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Ryan!')}>
-            </TouchableOpacity>
-            <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:windowHeight/2,left:windowWidth/2,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Siddha!')}>
-            </TouchableOpacity>
-          </View>
-        </Camera>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -36,4 +15,88 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cameraButton: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    bottom: 50, 
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderWidth: 5,
+    borderColor: 'gray'
+  },
+  faceContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: windowHeight,
+    width: windowWidth
+  }
 });
+
+const FaceComponent = face => {
+  const { rightEyePosition, leftEyePosition } = face.face;
+  console.log(rightEyePosition)
+  return (
+      <View>
+        <View style={{position: 'absolute', top: rightEyePosition.y,left: rightEyePosition.x,height:10,width:10,backgroundColor:'red'}}></View>
+      <View style={{position: 'absolute', top: leftEyePosition.y,left: leftEyePosition.x,height:10,width:10,backgroundColor:'red'}}></View>
+      </View>
+  )
+};
+
+export default class App extends React.Component {
+
+  state = {
+    faces: []
+  }
+
+  _renderButtons = () => (
+    <View>
+      <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:0,bottom:0,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Zach!')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:0,left:windowWidth/2,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Ryan!')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={{position:'absolute',backgroundColor:'transparent',top:windowHeight/2,left:windowWidth/2,height:windowHeight,width:windowWidth/2}} onPress={() => alert('Thats Siddha!')}>
+      </TouchableOpacity>
+    </View>
+  )
+
+  _takePicture = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync()
+    }
+  }
+
+  _handleFaceDetection = ({ faces }) => {
+    this.setState({ faces: faces })
+  }
+
+  render() {
+    const { faces } = this.state; 
+    return (
+      <View style={styles.container} >
+        <Camera
+          style={{ height: windowHeight, width: windowWidth }} 
+          type={Camera.Constants.Type.back}
+          ref={ref => this.camera = ref}
+          onFacesDetected={this._handleFaceDetection}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.all,
+            runClassifications: FaceDetector.Constants.Classifications.none}}>
+          
+        </Camera>
+        <View style={styles.faceContainer}>
+        {faces.map((face, index) => {
+            return (
+              <FaceComponent key={index} face={face}/>
+            )
+          })}
+          </View>
+        <TouchableOpacity style = {styles.cameraButton} onPress = {this._takePicture}></TouchableOpacity>
+
+      </View>
+    );
+  }
+}
